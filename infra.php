d07cc7c426488c206f1ff6adba137625592c3c65
Антон Кияткин <a.kiyatkin@itlf.ru>
2016-03-07 17:50:19 +0400

    fix isTest

--- infra.php
+++ infra.php
@@ -3,8 +3,9 @@ use infrajs\access\Access;
 use infrajs\ans\Ans;
 
 $action = Ans::GET('-access');
+
 if ($action == 'false') {
-	if (Access::test()) Access::$conf['test'] = true;
+	if (Access::isTest()) Access::$conf['test'] = true;
 	else Access::$conf['test'] = false; //Повышать права нельзя, если я не тестер, то нетестером и останусь!!!
 	Access::$conf['debug'] = false;
 	Access::$conf['admin'] = false;
