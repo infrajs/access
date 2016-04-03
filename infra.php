4540d79f0deddee68ab7cc8b29976d25ad986d9f
Anton <a.kiyatkin@itlf.ru>
2016-04-03 13:31:47 +0400

    -access=true

--- infra.php
+++ infra.php
@@ -9,4 +9,8 @@ if ($action == 'false') {
 	else Access::$conf['test'] = false; //Повышать права нельзя, если я не тестер, то нетестером и останусь!!!
 	Access::$conf['debug'] = false;
 	Access::$conf['admin'] = false;
+} else if ($action == 'true') {
+	Access::test(true);
+	Access::adminSetTime(); 
+	//Устанавливает будто админ только что заходил... это мягко обновит кэши шаблонов и проверит изменения файлов
 }
