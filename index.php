92a74302c6daea8d2003dd73870ada8a8e506ad0
Anton <a.kiyatkin@itlf.ru>
2016-05-03 15:02:52 +0400

    Запрос статуса кэш никогда не Public

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
 namespace infrajs\access;
 use infrajs\ans\Ans;
+use infrajs\nostore\Nostore;
 
 if(!is_file('vendor/autoload.php')) {
 	chdir('../../../');
 	require_once('vendor/autoload.php');
 }
 
-$ans=array();
+Nostore::on();
 
-
+$ans = array();
 $ans['test'] = Access::test();
 $ans['debug'] = Access::debug();
 $ans['admin'] = Access::admin();
