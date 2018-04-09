5c32bfa4f8e1d637fb413adc4227a2dcf98f4b40
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-04-09 18:39:47 +0400

    fix

--- Access.js
+++ Access.js
@@ -10,6 +10,10 @@ Access.debug = function () {
 Access.adminTime = function () {
 	return Load.loadJSON('-access/')['time'];
 }
+Access.getDebugTime = function () {
+	if (Access.debug()) return time();
+	else return Access.adminTime();
+}
 if (!window.infra) window.infra={}; if (!window.infrajs) window.infrajs={};
 infra.admin = Access.admin;
 infra.debug = Access.debug;
