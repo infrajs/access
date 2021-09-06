4c276f914a9839ac248e2aecf58a573eb5a52dc5
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2021-09-06 11:09:06 +0400

    fix

--- infra.php
+++ infra.php
@@ -9,14 +9,19 @@ $action = Ans::GET('-access');
 
 if ($action == 'false') {
 	if (Access::isTest()) Access::$conf['test'] = true;
-	else Access::$conf['test'] = false; //Повышать права нельзя, если я не тестер, то нетестером и останусь!!!
+	else Access::$conf['test'] = false; //Повышать права нельзя, если я не тестер, то не тестером и останусь!!!
 	Access::$conf['debug'] = false;
 	Access::$conf['admin'] = false;
+	Nostore::on();
+} else if ($action == 'update') {
+	Access::test(true);
+	Access::adminSetTime();
+	touch('composer.lock');
 	Nostore::on();
 } else if ($action == 'true') {
 	Access::test(true);
 	Access::adminSetTime();
-	touch('composer.lock');
+	//touch('composer.lock'); //Это будет обозначать сброс updateTime
 	Nostore::on();//Страница с таким параметром не кэшируется в браузере и её можно всегда спокойно вызывать
 	//Устанавливает будто админ только что заходил... это мягко обновит кэши шаблонов и проверит изменения файлов
 }
